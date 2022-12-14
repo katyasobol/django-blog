4cc313d7daa9c86f90e286520e3b38b920e56995
Ekaterina <katyshik2000@yandex.com>
2022-12-14 23:58:56 +0300

    commit

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,82 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import permissions, status, viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Category, Comment, Post
+from .serializers import (
+    CategoryReadSerializer,
+    CommentReadSerializer,
+    CommentWriteSerializer,
+    PostReadSerializer,
+    PostWriteSerializer,
+)
+
+from .permissions import IsAuthorOrReadOnly
+
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+
+    queryset = Category.objects.all()
+    serializer_class = CategoryReadSerializer
+    permission_classes = (permissions.AllowAny,)
+
+class PostViewSet(viewsets.ModelViewSet):
+
+    queryset = Post.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ("create", "update", "partial_update", "destroy"):
+            return PostWriteSerializer
+
+        return PostReadSerializer
+
+    def get_permissions(self):
+        if self.action in ("create",):
+            self.permission_classes = (permissions.IsAuthenticated,)
+        elif self.action in ("update", "partial_update", "destroy"):
+            self.permission_classes = (IsAuthorOrReadOnly,)
+        else:
+            self.permission_classes = (permissions.AllowAny,)
+
+        return super().get_permissions()
+
+class CommentViewSet(viewsets.ModelViewSet):
+
+    queryset = Comment.objects.all()
+
+    def get_queryset(self):
+        res = super().get_queryset()
+        post_id = self.kwargs.get("post_id")
+        return res.filter(post__id=post_id)
+
+    def get_serializer_class(self):
+        if self.action in ("create", "update", "partial_update", "destroy"):
+            return CommentWriteSerializer
+
+        return CommentReadSerializer
+
+    def get_permissions(self):
+        if self.action in ("create",):
+            self.permission_classes = (permissions.IsAuthenticated,)
+        elif self.action in ("update", "partial_update", "destroy"):
+            self.permission_classes = (IsAuthorOrReadOnly,)
+        else:
+            self.permission_classes = (permissions.AllowAny,)
+
+        return super().get_permissions()
+
+class LikePostAPIView(APIView):
+
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request, pk):
+        user = request.user
+        post = get_object_or_404(Post, pk=pk)
+
+        if user in post.likes.all():
+            post.likes.remove(user)
+
+        else:
+            post.likes.add(user)
+
+        return Response(status=status.HTTP_200_OK)
